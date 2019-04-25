217f86e6adbfc28fa8d7759c96c9a6644b68ea9c
Noel <noel@redmic.es>
2019-04-25 09:50:42 +0000

    Añade config para exportar métricas

--- src/main/java/es/redmic/notificationmanager/NotificationManagerApplication.java
+++ src/main/java/es/redmic/notificationmanager/NotificationManagerApplication.java
@@ -20,15 +20,28 @@ package es.redmic.notificationmanager;
  * #L%
  */
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 @ComponentScan({ "es.redmic.notificationmanager" })
 public class NotificationManagerApplication {
 
+	@Value("${info.microservice.name}")
+	String microserviceName;
+
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationManagerApplication.class, args);
+	}
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+		return registry -> registry.config().commonTags("application", microserviceName);
 	}
 }
